1ae3fae3403df47f1d35bfd97988a5fcb9d71297
SoulGamingYT <88420374+SoulGamingYT@users.noreply.github.com>
2025-08-28 14:35:08 +0500

    Update updates.js

--- api/updates.js
+++ api/updates.js
@@ -1,15 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-router.get('/', (req, res) => {
-  const updatesPath = path.join(__dirname, '../updates.json');
-  fs.readFile(updatesPath, 'utf8', (err, data) => {
-    if (err) return res.status(500).json({ error: 'Failed to read updates' });
-    const updates = JSON.parse(data);
-    res.json({ updates });
-  });
-});
-
-module.exports = router;
+export default function handler(req, res) {
+  const updatesPath = path.join(process.cwd(), 'updates.json');
+  const updates = JSON.parse(fs.readFileSync(updatesPath, 'utf8'));
+  res.json({ updates });
+}
